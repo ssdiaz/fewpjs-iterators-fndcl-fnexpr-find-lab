e2cce0c932782e4901173f7f4e31b2c11db5381e
Samantha Diaz <samidiaz3@gmail.com>
2022-01-27 22:43:28 -0500

    commit tree db2ce8b04c7564b6dbe70ca53926bb553e06adba

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+// // data looks like:
+// const record = [
+//     {year: "2018", result: "L"},
+//     {year: "2017", result: "W"},
+//     {year: "2016", result: "N/A"}
+//     //...
+//   ]
 
+superbowlWin = (array) => {
+    let win = array.find( superbowl => superbowl.result === 'W' )
+    return win ? win.year : undefined
+}
+
+// solution
+// superbowlWin = (record) => {
+//     let result = record.find( record => record.result === "W" )
+//     return !!result ? result.year : undefined
+//   }
